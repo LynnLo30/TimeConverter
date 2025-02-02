5ef69456cb4186203b162fc202f59d731eddc767
LynnLo30 <lynn.lo3005@gmail.com>
2025-02-02 23:36:17 +0800

    feat: display and choose date

--- script.js
+++ script.js
@@ -20,3 +20,63 @@ function handleScroll() {
 }
 
 window.addEventListener('scroll', handleScroll)
+
+// ----- handle time selector ----- //
+
+const date = new Date()
+const currentYear = date.getFullYear()
+const currentMonth = (date.getMonth() + 1).toString().padStart(2, '0')
+const currentDate = date.getDate().toString().padStart(2, '0')
+const currentDay = date.getDay()
+
+// ----- display current date
+
+const selectedDate = document.getElementById('selected')
+
+function getCurrentDate() {
+  selectedDate.innerText = `${currentYear}-${currentMonth}-${currentDate}`
+}
+
+// ----- click button to choose the date
+
+const dateSelector = document.querySelector('.date-selector')
+
+function chooseDisplayDate(e) {
+  let [year, month, date] = selectedDate.innerText.split('-').map(Number)
+
+  if (e.target.dataset.chevron === 'forward') {
+    date += 1
+  } else if (e.target.dataset.chevron === 'forward-double') {
+    date += 7
+  } else if (e.target.dataset.chevron === 'backward') {
+    date -= 1
+  } else if (e.target.dataset.chevron === 'backward-double') {
+    date -= 7
+  }
+
+  // month - 1: to match the date object's month index
+  const newDate = new Date(year, month - 1, date)
+  const chosenYear = newDate.getFullYear()
+  const chosenMonth = (newDate.getMonth() + 1).toString().padStart(2, '0')
+  const chosenDate = newDate.getDate().toString().padStart(2, '0')
+  const chosenDay = newDate.getDay()
+
+  selectedDate.innerText = `${chosenYear}-${chosenMonth}-${chosenDate}`
+
+  // when the date changes, day will also change
+  getCurrentDay(chosenDay)
+}
+
+const calendarPicker = document.getElementById('calendar-picker')
+
+function changeCalendarPicker() {
+  const pickedDate = this.value
+
+  // fix issue when user click the clear button will get an empty string
+  selectedDate.innerText =
+    pickedDate || `${currentYear}-${currentMonth}-${currentDate}`
+}
+
+getCurrentDate()
+dateSelector.addEventListener('click', chooseDisplayDate)
+calendarPicker.addEventListener('change', changeCalendarPicker)
