76c044b5c9520e0c5b783ba1c008e4a83645583a
LynnLo30 <lynn.lo3005@gmail.com>
2025-07-30 16:14:34 +0800

    refactor: organize JS logic into pure functions
    Completed all features of the time converter, including:
    - timezone conversion
    - copy-to-clipboard
    - real-time clock updates
    - user manual handling

--- script.js
+++ script.js
@@ -1,172 +1,254 @@
 // ----- handle header & footer ----- //
 
+function getPageState({
+  scrollY,
+  innerHeight,
+  bodyOffsetHeight,
+  arrowBtnTop,
+  arrowBtnHeight
+}) {
+  // if user hasn't scrolled the window or header isn't near the panel
+  const atTop = scrollY === 0 || arrowBtnTop <= arrowBtnHeight / 2
+  // if user has scrolled to the bottom of the page
+  const atBottom = scrollY + innerHeight >= bodyOffsetHeight
+
+  return {
+    headerShadow: !atTop,
+    footerShadow: !atBottom
+  }
+}
+
 function handleScroll() {
   const header = document.querySelector('.table-header')
   const arrowBtn = document.querySelector('.arrow-buttons')
   const footer = document.querySelector('.table-footer')
 
   // recalculated each time
-  const positionOfArrowBtn = arrowBtn.getBoundingClientRect()
+  const arrowBtnRect = arrowBtn.getBoundingClientRect()
 
-  // if user hasn't scrolled the window or arrowBtn has scrolled over the panel
-  const atTop =
-    window.scrollY === 0 || positionOfArrowBtn.top <= arrowBtn.clientHeight / 2
-  // if user has scrolled to the bottom of the page
-  const atBottom =
-    window.innerHeight + window.scrollY >= document.body.offsetHeight
+  const state = getPageState({
+    scrollY: window.scrollY,
+    innerHeight: window.innerHeight,
+    bodyOffsetHeight: document.body.offsetHeight,
+    arrowBtnTop: arrowBtnRect.top,
+    arrowBtnHeight: arrowBtn.clientHeight
+  })
 
-  header.classList.toggle('-shadow', !atTop)
-  footer.classList.toggle('-shadow', !atBottom)
+  header.classList.toggle('-shadow', state.headerShadow)
+  footer.classList.toggle('-shadow', state.footerShadow)
 }
 
-window.addEventListener('scroll', handleScroll)
+// ----- handle date selector ----- //
 
-// ----- handle time selector ----- //
+const dom = {
+  selectedDate: document.getElementById('selected'),
+  dateSelector: document.querySelector('.date-selector'),
+  calendarPicker: document.getElementById('calendar-picker'),
+  dayElms: document.querySelectorAll('.diffday'),
+  convertedTimeSum: document.querySelector('.convertedtime'),
+  clickToCopy: document.querySelector('.time-field'),
+  copyBtn: document.querySelector('.copybtn'),
+  copiedBtn: document.querySelector('.copiedbtn'),
+  copiedSvg: document.querySelector('.copiedsvg'),
+  panel: document.querySelector('.panel-container'),
+  tableBody: document.querySelector('.table-body'),
+  chevronButtons: document.querySelectorAll('[data-chevron]'),
+  resetBtn: document.querySelector('.resetbtn')
+}
 
-const date = new Date()
-const currentYear = date.getFullYear()
-const currentMonth = (date.getMonth() + 1).toString().padStart(2, '0')
-const currentDate = date.getDate().toString().padStart(2, '0')
-const currentDay = date.getDay()
+function getDateObj(dateObj = new Date()) {
+  return {
+    year: dateObj.getFullYear(),
+    month: (dateObj.getMonth() + 1).toString().padStart(2, '0'),
+    date: dateObj.getDate().toString().padStart(2, '0'),
+    day: dateObj.getDay()
+  }
+}
+
+function formatDate(dateObj) {
+  return `${dateObj.year}-${dateObj.month}-${dateObj.date}`
+}
+
+function formatTime({hours, minutes}) {
+  const hh = hours.toString().padStart(2, '0')
+  const mm = minutes.toString().padStart(2, '0')
+
+  return `${hh}:${mm}`
+}
 
 // ----- display current date
 
-const selectedDate = document.getElementById('selected')
-
-function getCurrentDate() {
-  selectedDate.innerText = `${currentYear}-${currentMonth}-${currentDate}`
-}
-
-// ----- click button to choose the date
-
-const dateSelector = document.querySelector('.date-selector')
-
-function chooseDisplayDate(e) {
-  let [year, month, date] = selectedDate.innerText.split('-').map(Number)
-
-  if (e.target.dataset.chevron === 'forward') {
-    date += 1
-  } else if (e.target.dataset.chevron === 'forward-double') {
-    date += 7
-  } else if (e.target.dataset.chevron === 'backward') {
-    date -= 1
-  } else if (e.target.dataset.chevron === 'backward-double') {
-    date -= 7
-  }
-
-  // month - 1: to match the date object's month index
-  const newDate = new Date(year, month - 1, date)
-  const chosenYear = newDate.getFullYear()
-  const chosenMonth = (newDate.getMonth() + 1).toString().padStart(2, '0')
-  const chosenDate = newDate.getDate().toString().padStart(2, '0')
-  const chosenDay = newDate.getDay()
-
-  selectedDate.innerText = `${chosenYear}-${chosenMonth}-${chosenDate}`
-
-  // when the date changes, day will also change
-  getCurrentDay(chosenDay)
-}
-
-const calendarPicker = document.getElementById('calendar-picker')
-
-function changeCalendarPicker() {
-  const pickedDate = this.value
-
-  // fix issue: when user click the clear button will get an empty string
-  selectedDate.innerText =
-    pickedDate || `${currentYear}-${currentMonth}-${currentDate}`
-
-  // fix issue: after user click the calendarPicker, day won't change
-  getCurrentDay(chosenDay)
+function displayCurrentDate(dateObj = getDateObj()) {
+  dom.selectedDate.textContent = formatDate(dateObj)
 }
 
 // ----- display current day
 
-function getCurrentDay(displayDay) {
+function getDayLabel(dayIndex) {
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  const dayElms = document.querySelectorAll('.diffday')
 
-  const dayIndex = displayDay !== undefined ? displayDay : currentDay
+  return daysOfWeek[dayIndex]
+}
 
-  dayElms.forEach((dayElm) => {
-    dayElm.textContent = daysOfWeek[dayIndex]
+function displayCurrentDay(dayIndex = getDateObj().day) {
+  dom.dayElms.forEach((dayElm) => {
+    dayElm.textContent = getDayLabel(dayIndex)
   })
+}
+
+// ----- click button to choose the date
+
+function chooseDisplayDateByBtn(e) {
+  let [year, month, date] = dom.selectedDate.textContent.split('-').map(Number)
+  let offset = 0
+
+  switch (e.target.dataset.chevron) {
+    case 'forward':
+      offset = 1
+      break
+    case 'forward-double':
+      offset = 7
+      break
+    case 'backward':
+      offset = -1
+      break
+    case 'backward-double':
+      offset = -7
+      break
+  }
+
+  // month - 1: to match the date object's month index
+  const chosenDateObj = getDateObj(new Date(year, month - 1, date + offset))
+
+  displayCurrentDate(chosenDateObj)
+  displayCurrentDay(chosenDateObj.day)
+}
+
+function changeByCalendarPicker() {
+  const pickedDate = this.value
+
+  // fix issue: when user click the clear button will get an empty string
+  const pickedDateObj = pickedDate
+    ? getDateObj(new Date(pickedDate))
+    : getDateObj()
+
+  displayCurrentDate(pickedDateObj)
+  displayCurrentDay(pickedDateObj.day)
 }
 
 // ----- display current time by timezone
 
-let timezone = [8, -4, 1]
-let baseLocalTime = ''
-let displayLocalTime = ''
+let modifiedTime = null
 
-function getCurrentTime(timezone) {
-  const getTimeByTimezone = (offset) => {
-    // make sure the display time is up-to-date
-    const now = new Date()
-    const utcHours = (now.getUTCHours() + offset).toString().padStart(2, '0')
-    const utcMinutes = now.getUTCMinutes().toString().padStart(2, '0')
+function getUtcTime(offset) {
+  const utcDateObj = new Date()
+  // fix issue: ensure that the hour is within th24-hour format
+  const hours = (utcDateObj.getUTCHours() + offset + 24) % 24
+  const minutes = utcDateObj.getUTCMinutes()
 
-    // fix issue: ensure that the hour is within the 24-hour format
-    if (utcHours < 0) {
-      utcHours += 24
-    } else if (utcHours >= 24) {
-      utcHours -= 24
+  return {hours, minutes}
+}
+
+function displayCurrentTime(timeObj = getUtcTime(8)) {
+  const panelElementIds = ['time-user', 'time-from', 'time-to']
+  const regions = ['Taiwan', 'New York', 'London']
+  const timeDiff = [0, -4, 1]
+
+  const baseMinutes = timeObj.hours * 60 + timeObj.minutes
+  const timeObjAry = timeDiff.map((offset) => {
+    const totalMinutes = (baseMinutes + offset * 60 + 1440) % 1440
+    return {
+      hours: Math.floor(totalMinutes / 60),
+      minutes: totalMinutes % 60
     }
-
-    return `${utcHours}:${utcMinutes}`
-  }
-
-  const elementIds = ['time-user', 'time-from', 'time-to']
-
-  elementIds.forEach((id, index) => {
-    document.getElementById(id).textContent = getTimeByTimezone(timezone[index])
   })
 
-  const convertedTime = document.querySelector('.convertedtime')
-  const [localTime, newYorkTime, londonTime] = timezone.map(getTimeByTimezone)
+  const convertedTimeAry = timeObjAry.map(formatTime)
 
-  convertedTime.textContent = `${localTime} (Taiwan) ／ ${newYorkTime} (New York) ／ ${londonTime} (London)`
+  panelElementIds.forEach((id, index) => {
+    document.getElementById(id).textContent = convertedTimeAry[index]
+  })
 
-  baseLocalTime = localTime
-  displayLocalTime = localTime
+  const summary = convertedTimeAry
+    .map((time, index) => `${time} (${regions[index]})`)
+    .join(' ／ ')
 
-  panelPosition(e, baseLocalTime)
+  dom.convertedTimeSum.textContent = summary
+
+  displayPanelPosition(timeObj)
 }
 
-function addMinutesToTime(timeStr, minutes) {
-  const [hour, min] = timeStr.split(':').map(Number)
-  const total = (hour * 60 + min + minutes + 1440) % 1440
-  const newHour = Math.floor(total / 60)
-    .toString()
-    .padStart(2, '0')
-  const newMin = (total % 60).toString().padStart(2, '0')
-  return `${newHour}:${newMin}`
+// ----- handle time panel ----- //
+
+// ----- calculate the height of the panel in the viewport
+
+const eachMinPx = parseFloat((dom.tableBody.clientHeight / 1440).toFixed(1))
+
+function getPanelPosition({hours, minutes}) {
+  return (hours * 60 + minutes) * eachMinPx
 }
 
-// ----- copy the converted time
+// ----- adjust panel position by time
 
-const clickToCopy = document.querySelector('.time-field')
+function displayPanelPosition(timeObj) {
+  let position = Math.round(getPanelPosition(timeObj) * 10) / 10
+
+  // set the min & max values ​​of the panel position
+  position = Math.min(
+    Math.max(position, 0),
+    dom.tableBody.clientHeight - dom.panel.clientHeight
+  )
+
+  dom.panel.dataset.position = position
+  dom.panel.style.top = `${position}px`
+}
+
+// ----- click arrow-buttons to adjust the panel position and time
+
+function handleTimePanel(e) {
+  const step = 15
+  const direction = e.target.dataset.chevron === 'angle-up' ? -1 : 1
+  const baseTime = modifiedTime || getUtcTime(8) // currentTime
+  const baseTotalMin = baseTime.hours * 60 + baseTime.minutes
+  const checkTotalMin = baseTotalMin + direction * step
+
+  if (checkTotalMin < 0 || checkTotalMin >= 1440) return
+
+  modifiedTime = {
+    hours: Math.floor(checkTotalMin / 60),
+    minutes: checkTotalMin % 60
+  }
+
+  displayCurrentTime(modifiedTime)
+  dom.resetBtn.style.visibility = 'visible'
+}
+
+// ----- reset the time on the panel by button
+function resetPanel() {
+  modifiedTime = null
+  displayCurrentTime()
+
+  delete dom.panel.dataset.position
+  dom.resetBtn.style.visibility = 'hidden'
+}
+
+// ----- copy the converted time summary
 
 async function copyConvertedTime(e) {
-  const copyBtn = document.querySelector('.copybtn')
-  const copiedBtn = document.querySelector('.copiedbtn')
-  const copiedSvg = document.querySelector('.copiedsvg')
-  const textToCopy = document.querySelector('.convertedtime').innerText
-
   const showSuccess = () => {
-    copyBtn.classList.add('-hidden')
-    copiedBtn.classList.remove('-hidden')
-    copiedSvg.classList.remove('-hidden')
+    dom.copyBtn.classList.add('-hidden')
+    dom.copiedBtn.classList.remove('-hidden')
+    dom.copiedSvg.classList.remove('-hidden')
   }
 
   const resetToDefault = () => {
-    copyBtn.classList.remove('-hidden')
-    copiedBtn.classList.add('-hidden')
-    copiedSvg.classList.add('-hidden')
+    dom.copyBtn.classList.remove('-hidden')
+    dom.copiedBtn.classList.add('-hidden')
+    dom.copiedSvg.classList.add('-hidden')
   }
 
   try {
-    await navigator.clipboard.writeText(textToCopy)
+    await navigator.clipboard.writeText(dom.convertedTimeSum.textContent)
     showSuccess()
 
     setTimeout(() => {
@@ -177,76 +259,23 @@ async function copyConvertedTime(e) {
   }
 }
 
-// ----- handle the panel ----- //
+window.addEventListener('scroll', handleScroll)
 
-// ----- calculate the height of the panel in the viewport
-const panel = document.querySelector('.panel-container')
-
-function panelPosition(e, time) {
-  const tableBody = document.querySelector('.table-body')
-  const resetBtn = document.querySelector('.resetbtn')
-
-  // original panel position
-  const eachMinPx = parseFloat((tableBody.clientHeight / 1440).toFixed(1))
-  const localTimeArr = time.split(':').map(Number)
-  const initialPosition = (localTimeArr[0] * 60 + localTimeArr[1]) * eachMinPx
-  const step = eachMinPx * 15
-
-  // fix issue: the panel will jump back to its initial position due to setInterval
-  if (!panel.dataset.position) {
-    panel.dataset.position = initialPosition
-    panel.style.top = `${initialPosition}px`
+displayCurrentDate()
+displayCurrentDay()
+setInterval(() => {
+  if (!modifiedTime) {
+    displayCurrentTime()
   }
+}, 1000)
 
-  // set the min & max values ​​of the panel position
-  panel.dataset.position = Math.min(
-    Math.max(parseFloat(panel.dataset.position), 0),
-    tableBody.clientHeight - panel.clientHeight
-  )
-
-
-  // click arrow-buttons to adjust the panel position
-  // improve：視窗會跟著按鈕移動
-  // error: 時間會跟著按鈕改變
-
-  let adjustMinutes = 0
-
-  if (e.target.dataset.chevron === 'angle-up') {
-    panel.dataset.position = parseFloat(panel.dataset.position) - step
-    adjustMinutes = -15
-  } else if (e.target.dataset.chevron === 'angle-down') {
-    panel.dataset.position = parseFloat(panel.dataset.position) + step
-    adjustMinutes = 15
-  }
-
-  if (adjustMinutes !== 0) {
-    displayLocalTime = addMinutesToTime(baseLocalTime, adjustMinutes)
-
-    resetBtn.style.visibility = 'visible'
-  }
-
-  // update the position according to data-position
-  panel.style.top = `${panel.dataset.position}px`
-}
-
-function resetPanel() {
-  displayLocalTime = baseLocalTime
-  delete panel.dataset.position
-
-  getCurrentTime(timezone)
-  resetBtn.style.visibility = 'hidden'
-}
-
-getCurrentDate()
-getCurrentDay()
-
-setInterval(() => getCurrentTime(timezone), 1000) // improve: performance issues
-dateSelector.addEventListener('click', chooseDisplayDate)
-/* fix issue: the calendarPicker doesn't update immediately after selecting a date.
+dom.dateSelector.addEventListener('click', chooseDisplayDateByBtn)
+/* fix issue: 
+the calendarPicker doesn't update immediately after selecting a date.
 Use the input event instead of change. */
-calendarPicker.addEventListener('input', changeCalendarPicker)
-clickToCopy.addEventListener('click', copyConvertedTime)
-panel.addEventListener('click', (e) => {
-  panelPosition(e, displayLocalTime)
+dom.calendarPicker.addEventListener('input', changeByCalendarPicker)
+dom.clickToCopy.addEventListener('click', copyConvertedTime)
+dom.chevronButtons.forEach((btn) => {
+  btn.addEventListener('click', handleTimePanel)
 })
-resetBtn.addEventListener('click', resetPanel)
+dom.resetBtn.addEventListener('click', resetPanel)
